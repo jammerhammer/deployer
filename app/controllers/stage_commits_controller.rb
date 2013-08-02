9a2b2e6ef8d7c9a797a3f8dbac34f9ba2aada04a
Jason Woodlee <jwoodlee99@gmail.com>
2013-08-02 09:26:24 -0400

    Initial Working Code
    - Added config
    - Only shows non reviewed
    - Squashs Reviews
    - Pushes like boss

--- app/controllers/stage_commits_controller.rb
+++ app/controllers/stage_commits_controller.rb
@@ -6,6 +6,9 @@ class StageCommitsController < ApplicationController
   # GET /stage_commits.json
   def index
     @git_commits = GitHubber.all
+    commit_hashes = @git_commits.map { |commit| commit.sha }
+    approved_commits = StageCommit.where("sha in (?)",  commit_hashes).map{ |reviews| reviews.sha }
+    @git_commits.reject!{|git_commit| approved_commits.include?(git_commit.sha) }
     @stage_commit = StageCommit.new
   end
 
@@ -22,6 +25,14 @@ class StageCommitsController < ApplicationController
     respond_to do |format|
       if @stage_commit.save
         GitHubber.promote_to_prod(@stage_commit.sha)
+        old_commits=GitHubber.all_before_hash(@stage_commit.sha)
+        logged_commits = StageCommit.where("sha in (?)", old_commits).map{ |reviews| reviews.sha }
+        skipped_commits = old_commits - logged_commits
+
+        skipped_commits.each do |skipped_sha|
+          StageCommit.create(:sha => skipped_sha, :approver => @stage_commit.approver, :comment => "Auto Approved in Commit #{@stage_commit.sha} and Message #{@stage_commit.comment}")
+        end
+
         format.html { redirect_to @stage_commit, notice: 'Stage commit was successfully created.' }
         format.json { render action: 'show', status: :created, location: @stage_commit }
       else
@@ -41,6 +52,4 @@ private
   def stage_commit_params
     params.require(:stage_commit).permit(:sha,:email, :commit_stamp, :approved, :approver, :comment, :developer)
   end
-
-
 end
